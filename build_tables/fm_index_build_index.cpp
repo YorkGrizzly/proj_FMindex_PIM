d9e3bb3e605b6a30ad4265b0c20ad78c7ab502e5
YorkGrizzly <yorkygrizzly@gmail.com>
2021-04-19 09:26:02 +0200

    build tables with 640 reads

--- build_tables/fm_index_build_index.cpp
+++ build_tables/fm_index_build_index.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -26,7 +26,7 @@ std::vector<std::string> rotate_and_sort(const std::string &reference_string)
 
     std::string current_rotation = reference_string;
     // std::cout << current_rotation.size() << std::endl;
-    for(size_t i = 0; i < reference_string.length(); ++i)
+    for (size_t i = 0; i < reference_string.length(); ++i)
     {
         current_rotation.push_back(current_rotation[0]);
         current_rotation.erase(current_rotation.begin());
@@ -39,24 +39,24 @@ std::vector<std::string> rotate_and_sort(const std::string &reference_string)
 uint32_t encode_to_int(std::string string_to_encode)
 {
     uint32_t encoded_value = 0;
-    for(int i = string_to_encode.size() - 1; i >= 0; --i)
+    for (int i = string_to_encode.size() - 1; i >= 0; --i)
     {
-        switch(string_to_encode[i])
+        switch (string_to_encode[i])
         {
-            case '$':
-                // encoded_value += 0 * pow(5, i);
+        case '$':
+            // encoded_value += 0 * pow(5, i);
             break;
-            case 'A':
-                encoded_value += 1 * pow(5, string_to_encode.size() - 1 - i);
+        case 'A':
+            encoded_value += 1 * pow(5, string_to_encode.size() - 1 - i);
             break;
-            case 'C':
-                encoded_value += 2 * pow(5, string_to_encode.size() - 1 - i);
+        case 'C':
+            encoded_value += 2 * pow(5, string_to_encode.size() - 1 - i);
             break;
-            case 'G':
-                encoded_value += 3 * pow(5, string_to_encode.size() - 1 - i);
+        case 'G':
+            encoded_value += 3 * pow(5, string_to_encode.size() - 1 - i);
             break;
-            case 'T':
-                encoded_value += 4 * pow(5, string_to_encode.size() -1 - i);
+        case 'T':
+            encoded_value += 4 * pow(5, string_to_encode.size() - 1 - i);
             break;
         }
     }
@@ -66,32 +66,32 @@ uint32_t encode_to_int(std::string string_to_encode)
 std::string decode_to_string(uint32_t int_to_decode)
 {
     std::string decoded_string;
-    for(size_t i = 0; i < STEP; ++i)
+    for (size_t i = 0; i < STEP; ++i)
     {
-        switch((int_to_decode / (int)pow(5, STEP - 1 - i)))
+        switch ((int_to_decode / (int)pow(5, STEP - 1 - i)))
         {
-            case 0:
-                decoded_string.push_back('$');
+        case 0:
+            decoded_string.push_back('$');
             break;
 
-            case 1:
-                decoded_string.push_back('A');
+        case 1:
+            decoded_string.push_back('A');
             break;
 
-            case 2:
-                decoded_string.push_back('C');
+        case 2:
+            decoded_string.push_back('C');
             break;
 
-            case 3:
-                decoded_string.push_back('G');
+        case 3:
+            decoded_string.push_back('G');
             break;
 
-            case 4:
-                decoded_string.push_back('T');
+        case 4:
+            decoded_string.push_back('T');
             break;
 
-            default:
-                std::cout << "UNRECOGNIZED CHARACTER DURING DECODE!!\n";
+        default:
+            std::cout << "UNRECOGNIZED CHARACTER DURING DECODE!!\n";
         }
         int_to_decode %= (int)pow(5, STEP - 1 - i);
     }
@@ -102,12 +102,12 @@ void extract_tables(const std::vector<std::string> &rotated_and_sorted_strings, 
 {
     std::vector<uint32_t> running_occ(pow(5, STEP), 0);
     // std::cout << running_occ.size() << std::endl;
-    for(size_t i = 0; i < rotated_and_sorted_strings.size(); ++i)
+    for (size_t i = 0; i < rotated_and_sorted_strings.size(); ++i)
     {
         // F_offsets
         std::string F_string = rotated_and_sorted_strings[i].substr(0, STEP);
         uint32_t encoded_F = encode_to_int(F_string);
-        if(F_offsets[encoded_F] == 0)
+        if (F_offsets[encoded_F] == 0)
         {
             F_offsets[encoded_F] = i + 1;
         }
@@ -118,89 +118,91 @@ void extract_tables(const std::vector<std::string> &rotated_and_sorted_strings, 
 
         // occ_table
         ++running_occ[encoded_L];
-        if(i % OCC_SAMPLING_DIST == 0)
+        if (i % OCC_SAMPLING_DIST == 0)
         {
             // std::cout << "sampled at " <<  i << std::endl;
             occ_table.push_back(running_occ);
         }
     }
-
 }
-
 
 int main()
 {
-    const std::string reference_string = "ATCGAGCGCGCATCG$";
-    std::cout << "size of reference: " << reference_string.length() << std::endl;
+    std::ifstream in_DNAReadsFile("../dataset/extracted_sequences.txt");
+    std::ofstream out_TableFile("../table.txt");
 
-    std::vector<std::string> sorted_strings = rotate_and_sort(reference_string);
-    std::vector<size_t> F_offsets(pow(5, STEP), 0);
-    std::vector<uint32_t> L_column;
-    std::vector<std::vector<uint32_t>> occ_table;
+    // const std::string reference_string = "ATCGAGCGCGCATCG$";
+    // std::cout << "size of reference: " << reference_string.length() << std::endl;
 
-    // print rotated strings
-    for(size_t i = 0; i < sorted_strings.size(); ++i)
+
+    // rotate and sort reads from table.txt
+    for (std::string read; getline(in_DNAReadsFile, read);)
     {
-        std::cout << i << ": ";
-        std::cout << sorted_strings[i] << std::endl;
-    }
-   
-    extract_tables(sorted_strings, F_offsets, L_column, occ_table);
+        std::vector<std::string> sorted_strings = rotate_and_sort(read);
 
-    // print F offsets
-    std::cout << "printing F offsets:" << std::endl;
-    for(size_t i = 0; i < F_offsets.size(); ++i)
-    {
-        if(F_offsets[i])
+        std::vector<size_t> F_offsets(pow(5, STEP), 0);
+        std::vector<uint32_t> L_column;
+        std::vector<std::vector<uint32_t>> occ_table;
+
+        // print rotated strings
+        for (size_t i = 0; i < sorted_strings.size(); ++i)
         {
-            std::cout << decode_to_string(i) << ": ";
-            std::cout << F_offsets[i] << std::endl;
+            std::cout << i << ": ";
+            std::cout << sorted_strings[i] << std::endl;
         }
-    }
 
-    // print L column
-    std::cout << "printing L column:" << std::endl;
-    for(uint32_t a : L_column)
-    {
-        std::cout << decode_to_string(a) << std::endl;
-        // std::cout << a << std::endl;
-    }
+        extract_tables(sorted_strings, F_offsets, L_column, occ_table);
 
-    // print occ table
-    std::cout << "printing Occ table:" << std::endl;
-    for(size_t i = 0; i < occ_table[0].size(); ++i)
-    {
-        std::cout << decode_to_string(i) << " ";
-    }
-    std::cout << std::endl;
-    for(auto &occ_entry : occ_table)
-    {
-        for(uint32_t &occ : occ_entry)
+        // print F offsets
+        std::cout << "printing F offsets:" << std::endl;
+        for (size_t i = 0; i < F_offsets.size(); ++i)
         {
-            std::cout << occ << std::string(STEP, ' ');
+            if (F_offsets[i])
+            {
+                std::cout << decode_to_string(i) << ": ";
+                std::cout << F_offsets[i] << std::endl;
+            }
+        }
+
+        // print L column
+        std::cout << "printing L column:" << std::endl;
+        for (uint32_t a : L_column)
+        {
+            std::cout << decode_to_string(a) << std::endl;
+            // std::cout << a << std::endl;
+        }
+
+        // print occ table
+        std::cout << "printing Occ table:" << std::endl;
+        for (size_t i = 0; i < occ_table[0].size(); ++i)
+        {
+            std::cout << decode_to_string(i) << " ";
         }
         std::cout << std::endl;
-    }
-
-
-
-    // write to files
-    std::fstream table;
-    table.open("table.txt", std::ios::out);
-    for(int a : F_offsets)
-    {
-        table << a << " ";
-    }
-    for(uint32_t a : L_column)
-    {
-        table << a << " ";
-    }
-    for(auto &occ_entry : occ_table)
-    {
-        for(uint32_t &occ : occ_entry)
+        for (auto &occ_entry : occ_table)
         {
-            table << occ << " ";
+            for (uint32_t &occ : occ_entry)
+            {
+                std::cout << occ << std::string(STEP, ' ');
+            }
+            std::cout << std::endl;
         }
+
+        for (int a : F_offsets)
+        {
+            out_TableFile << a << " ";
+        }
+        for (uint32_t a : L_column)
+        {
+            out_TableFile << a << " ";
+        }
+        for (auto &occ_entry : occ_table)
+        {
+            for (uint32_t &occ : occ_entry)
+            {
+                out_TableFile << occ << " ";
+            }
+        }
+        out_TableFile << "\n";
     }
-    table << "\n";
 }
