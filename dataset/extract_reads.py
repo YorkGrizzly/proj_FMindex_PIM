3f96a528acc35a19b8b4b37d8fe2d900500a29d3
YorkGrizzly <yorkygrizzly@gmail.com>
2021-04-22 19:39:17 -0400

    new datasets, modified extraction program

--- dataset/extract_reads.py
+++ dataset/extract_reads.py
@@ -1,20 +1,18 @@
 import random
 
-selected_reads = random.sample(range(0, 999999), 640)
+nb_of_extracted_reads = 640000
 
 fastq_input = open("ERR174310.fastq", "r")
-output_file = open("extracted.fastq", "w")
-output_file2 = open("extracted_sequences.txt", "w")
+selected_reads = random.sample(range(0, 999999), nb_of_extracted_reads)
+
+output_file = open("extracted_" + str(nb_of_extracted_reads) + ".fastq", "w")
+output_file2 = open("extracted_sequences_" + str(nb_of_extracted_reads) + ".txt", "w")
 
 for line_nb, line in enumerate(fastq_input):
     if line_nb // 4 in selected_reads:
         output_file.write(line)
     if (line_nb - 1) / 4 in selected_reads:
         output_file2.write(line)
-        
 
-        
-
-
-fastq_input.close()
 output_file.close()
+fastq_input.close()
