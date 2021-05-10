77782a49348f865e67bcc9c44c9eccbc2c834950
YorkGrizzly <yorkygrizzly@gmail.com>
2021-05-10 03:07:23 -0400

    add 1000000 extracted

--- dataset/extract_reads.py
+++ dataset/extract_reads.py
@@ -1,26 +1,25 @@
 import random
 
-nb_of_extracted_reads = 640000
+nb_of_extracted_reads = 1000000
 
-for nb_of_extracted_reads in [100000]:
-    print("extracting " + str(nb_of_extracted_reads) + " reads...")
-    fastq_input = open("ERR174310.fastq", "r")
-    selected_reads = random.sample(range(0, 999999), nb_of_extracted_reads)
-    selected_reads.sort()
+fastq_input = open("ERR174310.fastq", "r")
+# selected_reads = random.sample(range(0, 999999), nb_of_extracted_reads)
+# selected_reads.sort()
+selected_reads = list(range(1000000))
 
-    output_file = open("extracted_" + str(nb_of_extracted_reads) + ".fastq", "w")
-    output_file2 = open("extracted_sequences_" + str(nb_of_extracted_reads) + ".txt", "w")
+output_file = open("extracted_" + str(nb_of_extracted_reads) + ".fastq", "w")
+output_file2 = open("extracted_sequences_" + str(nb_of_extracted_reads) + ".txt", "w")
 
-    for line_nb, line in enumerate(fastq_input):
-        if not selected_reads:
-            break
-        if line_nb // 4 == selected_reads[0]:
-            output_file.write(line)
-        if (line_nb - 1) / 4 == selected_reads[0]:
-            output_file2.write(line)
-        if line_nb // 4 > selected_reads[0]:
-            selected_reads.pop(0)
-    
+for line_nb, line in enumerate(fastq_input):
+    if not selected_reads:
+        break
+    if line_nb // 4 == selected_reads[0]:
+        output_file.write(line)
+    if (line_nb - 1) / 4 == selected_reads[0]:
+        output_file2.write(line)
+    if line_nb // 4 > selected_reads[0]:
+        selected_reads.pop(0)
 
-    output_file.close()
-    fastq_input.close()
+
+output_file.close()
+fastq_input.close()
